73e250d4d5cafcc2b55ee921cfd5ca07be423fa4
LiliiaB <74778217+LiliiaB@users.noreply.github.com>
2024-01-28 11:50:51 +0000

    Deploying to gh-pages from @ LiliiaB/goit-js-hw-10@9fa6031680600510a74f785aebee06342eafd0ec 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-94c342ae.js";function n(o){const t=Math.floor(o/864e5),c=Math.floor(o%864e5/36e5),s=Math.floor(o%864e5%36e5/6e4),r=Math.floor(o%864e5%36e5%6e4/1e3);return{days:t,hours:c,minutes:s,seconds:r}}console.log(n(2e3));console.log(n(14e4));console.log(n(2414e4));
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as t}from"./assets/vendor-cd4b7665.js";let e=document.querySelector("button");e.disabled=!0;e.addEventListener("click",o);function o(){t.show({message:"Please choose a date in the future"}),console.log("button does not work")}
 //# sourceMappingURL=commonHelpers.js.map
