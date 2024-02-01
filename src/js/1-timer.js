1b8948e8985100840ac90c3e71d5fe94f4575acc
Lilia Levchenko <liliya.benatti@gmail.com>
2024-02-01 12:30:01 +0100

    upd task1 countdown

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,24 +6,41 @@ import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
 
-let myButtonEl = document.querySelector('button');
-myButtonEl.disabled = true;
-myButtonEl.addEventListener('click', showAlert);
-
-function showAlert() {
-  iziToast.show({
-    message: 'Please choose a date in the future',
-  });
-  console.log('button does not work');
-}
-/* 
-function handleClick() {
-  myButtonEl.classList.toggle('disable');
-}
-
-const fp = flatpickr('#datetime-picker', options);
+const myButtonEl = document.querySelector('button');
+const timerEl = document.querySelector('.timer');
+const daysSpan = timerEl.querySelector('.value[data-days]');
+const hoursSpan = timerEl.querySelector('.value[data-hours]');
+const minutesSpan = timerEl.querySelector('.value[data-minutes]');
+const secondsSpan = timerEl.querySelector('.value[data-seconds]');
 
 let userSelectedDate;
+let countdownInterval;
+
+myButtonEl.addEventListener('click', () => {
+  userSelectedDate = new Date(instance.selectedDates[0]);
+
+  const updateCountdown = () => {
+    const now = new Date();
+    const remainingTime = userSelectedDate - now;
+    if (remainingTime <= 0) {
+      clearInterval(countdownInterval);
+      onTick(0);
+      updateButtonState();
+    } else {
+      onTick(remainingTime);
+    }
+  };
+  updateCountdown();
+  countdownInterval = setInterval(updateCountdown, 1000);
+});
+
+function onTick(remainingTime) {
+  const { days, hours, minutes, seconds } = convertMs(remainingTime);
+  daysSpan.textContent = days;
+  hoursSpan.textContent = hours;
+  minutesSpan.textContent = minutes;
+  secondsSpan.textContent = seconds;
+}
 
 const options = {
   enableTime: true,
@@ -32,9 +49,30 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    updateButtonState();
     console.log(selectedDates[0]);
   },
 };
+
+function updateButtonState() {
+  if (userSelectedDate < new Date()) {
+    iziToast.error({
+      position: 'topRight',
+      message: 'Please choose a date in the future',
+    });
+    myButtonEl.disabled = true;
+    myButtonEl.style.color = '#989898';
+    myButtonEl.style.background = '#cfcfcf';
+    myButtonEl.style.pointerEvents = 'none';
+  } else {
+    myButtonEl.disabled = false;
+    myButtonEl.style.color = '';
+    myButtonEl.style.background = '';
+    myButtonEl.style.pointerEvents = '';
+  }
+}
+const instance = flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -44,13 +82,21 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = Math.floor(ms / day)
+    .toString()
+    .padStart(2, '0');
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = Math.floor((ms % day) / hour)
+    .toString()
+    .padStart(2, '0');
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = Math.floor(((ms % day) % hour) / minute)
+    .toString()
+    .padStart(2, '0');
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second)
+    .toString()
+    .padStart(2, '0');
 
   return { days, hours, minutes, seconds };
 }
@@ -58,6 +104,3 @@ function convertMs(ms) {
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-addLeadingZero(value);
- */
