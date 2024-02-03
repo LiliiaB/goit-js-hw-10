f04532705453bdf5c6b15cfc2b3741f713532754
Lilia Levchenko <liliya.benatti@gmail.com>
2024-02-03 19:00:02 +0100

    rework

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,46 +1,46 @@
 import flatpickr from 'flatpickr';
-
-import 'flatpickr/dist/flatpickr.min.css';
-
 import iziToast from 'izitoast';
 
+import 'flatpickr/dist/flatpickr.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const myButtonEl = document.querySelector('button');
+const inputEl = document.querySelector('#datetime-picker');
 const timerEl = document.querySelector('.timer');
-const daysSpan = timerEl.querySelector('.value[data-days]');
-const hoursSpan = timerEl.querySelector('.value[data-hours]');
-const minutesSpan = timerEl.querySelector('.value[data-minutes]');
-const secondsSpan = timerEl.querySelector('.value[data-seconds]');
+const daysSpan = timerEl.querySelector('[data-days]');
+const hoursSpan = timerEl.querySelector('[data-hours]');
+const minutesSpan = timerEl.querySelector('[data-minutes]');
+const secondsSpan = timerEl.querySelector('[data-seconds]');
 
 let userSelectedDate;
 let countdownInterval;
+myButtonEl.disabled = true;
 
-myButtonEl.addEventListener('click', () => {
-  userSelectedDate = new Date(instance.selectedDates[0]);
-
-  const updateCountdown = () => {
-    const now = new Date();
-    const remainingTime = userSelectedDate - now;
-    if (remainingTime <= 0) {
-      clearInterval(countdownInterval);
-      onTick(0);
-      updateButtonState();
-    } else {
-      onTick(remainingTime);
-    }
-  };
-  updateCountdown();
+function startCountdown() {
   countdownInterval = setInterval(updateCountdown, 1000);
-});
+}
 
-function onTick(remainingTime) {
+function updateCountdown() {
+  const now = new Date();
+  const remainingTime = userSelectedDate - now;
+  if (remainingTime <= 0) {
+    stopCountdown();
+    return;
+  }
   const { days, hours, minutes, seconds } = convertMs(remainingTime);
   daysSpan.textContent = days;
   hoursSpan.textContent = hours;
   minutesSpan.textContent = minutes;
   secondsSpan.textContent = seconds;
 }
+
+myButtonEl.addEventListener('click', () => {
+  if (userSelectedDate) {
+    myButtonEl.disabled = true;
+    inputEl.disabled = true;
+    startCountdown();
+  }
+});
 
 const options = {
   enableTime: true,
@@ -50,29 +50,29 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     userSelectedDate = selectedDates[0];
-    updateButtonState();
-    console.log(selectedDates[0]);
+    if (userSelectedDate < Date.now()) {
+      iziToast.error({
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+      myButtonEl.disabled = true;
+    } else {
+      myButtonEl.disabled = false;
+    }
   },
 };
 
-function updateButtonState() {
-  if (userSelectedDate < new Date()) {
-    iziToast.error({
-      position: 'topRight',
-      message: 'Please choose a date in the future',
-    });
-    myButtonEl.disabled = true;
-    myButtonEl.style.color = '#989898';
-    myButtonEl.style.background = '#cfcfcf';
-    myButtonEl.style.pointerEvents = 'none';
-  } else {
-    myButtonEl.disabled = false;
-    myButtonEl.style.color = '';
-    myButtonEl.style.background = '';
-    myButtonEl.style.pointerEvents = '';
-  }
+function stopCountdown() {
+  clearInterval(countdownInterval);
+  daysSpan.textContent = '00';
+  hoursSpan.textContent = '00';
+  minutesSpan.textContent = '00';
+  secondsSpan.textContent = '00';
+  countdownInterval = null;
+  inputEl.disabled = false;
 }
-const instance = flatpickr('#datetime-picker', options);
+
+const instance = flatpickr(inputEl, options);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
